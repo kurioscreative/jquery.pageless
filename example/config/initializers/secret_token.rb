e398f3828d018f6c49fcaca717293af9abc74fc1
Kurios Creative Design <glenn@kurioscreative.com>
2011-06-12 21:21:52 -0400

    Replaced Rails 2.2 with Rails 3 example. Added installation instruction.

---
+++ example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PagelessExample::Application.config.secret_token = '94fa98cf43698245de0d87fac7ecfa75cdfa65d0187cbff8d06ab8808f61a0238c219e7ebe7679698994f6258aa54d2f8a3ad43aae4e4becd01bf4423483e743'
